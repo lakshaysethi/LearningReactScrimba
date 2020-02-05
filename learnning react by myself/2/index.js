48e92ab76912541d31eb6004c1fbf51efa1f660b
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-02-05 16:27:07 +1300

    3 divs color

--- learnning react by myself/2/index.js
+++ learnning react by myself/2/index.js
@@ -1,47 +1,45 @@
-const todosData = [
-  {
-    id: 1,
-    text: "Take out the trash",
-    completed: true
-  },
-  {
-    id: 2,
-    text: "Grocery shopping",
-    completed: false
-  },
-  {
-    id: 3,
-    text: "Clean gecko tank",
-    completed: false
-  },
-  {
-    id: 4,
-    text: "Mow lawn",
-    completed: true
-  },
-  {
-    id: 5,
-    text: "Catch up on Arrested Development",
-    completed: false
+class ColorDiv extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { color: this.props.color };
+    this.changeColor = this.changeColor.bind(this);
   }
-];
-
-function TodoItem(props) {
-  return (
-    <div id="todo-item" className="todo-item">
-      <input type="checkbox" checked={props.item.completed} />
-      <p>{props.item.text}</p>
-    </div>
-  );
+  changeColor() {
+    this.setState({ color: "red" });
+  }
+  render() {
+    let style = {
+      backgroundColor: this.state.color,
+      minHeight: 100,
+      Width: 100,
+      border: "1px solid",
+      display: "inline-block"
+    };
+    return (
+      <div
+        style={style}
+        className="color-div"
+        id={`${this.state.color}-color-div`}
+      >
+        <div style={style}></div>
+        <button onClick={this.changeColor}>
+          Change color Of {`${this.state.color}-color-div`}
+        </button>
+      </div>
+    );
+  }
+}
+class App extends React.Component {
+  render() {
+    return (
+      <div className="color-Divs-Holder">
+        <ColorDiv key={1} color="green" />
+        <ColorDiv key={2} color="black" />
+        <ColorDiv key={3} color="blue" />
+      </div>
+    );
+  }
 }
 
-function App() {
-  const todoItems = todosData.map(item => {
-    return <TodoItem key={item.id} item={item} />;
-  });
-
-  return <div className="todo-list">{todoItems}</div>;
-}
-
-ReactDOM.render(<App />, document.getElementById("#root"));
-// $("#root div ul li .to-do")
+ReactDOM.render(<App />, document.getElementById("root"));
